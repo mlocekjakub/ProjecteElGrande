aa3f4ccccc2b0700949b5ef877db0d66322d9367
marcinZyla13 <82803199+marcinZyla13@users.noreply.github.com>
2022-02-15 22:44:11 +0100

    Model attributes implementation

--- KeepMovinAPI/KeepMovinAPI/Models/User.cs
+++ KeepMovinAPI/KeepMovinAPI/Models/User.cs
@@ -12,12 +12,13 @@ namespace KeepMovinAPI.Models
         public int userid { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
-        [StringLength(int.MaxValue, MinimumLength = 6, ErrorMessage = "Email must have at least 6 characters")]
+        [StringLength(200, ErrorMessage = "Email must be between 6 and 60 signs ", MinimumLength = 6)]
+        [EmailAddress]
         [Column("email")]
 		public string Email { get;set; }
 
-        [Required(ErrorMessage = "Password name is required")]
-        [StringLength(int.MaxValue, MinimumLength = 6, ErrorMessage = "Password must have at least 6 characters")]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(200,ErrorMessage = "Password length must be between 6 and 30 signs", MinimumLength = 6)]
         [Column("password")]
 		public string Password { get;set;}
 
