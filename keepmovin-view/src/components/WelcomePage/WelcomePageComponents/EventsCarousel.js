40faaa21739964412de56cf8a08df5ae10e348a8
JakJar <kuba.jaroszewicz@gmail.com>
2022-02-17 11:04:37 +0100

    Merge

--- keepmovin-view/src/components/WelcomePage/WelcomePageComponents/EventsCarousel.js
+++ keepmovin-view/src/components/WelcomePage/WelcomePageComponents/EventsCarousel.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './EventCarouselStyles.css';
 import {ArrowBack, ArrowForward} from "@material-ui/icons";
-import EventImage from '../../../Images/News-Trailer-Web-4Sep20.png';
+import CarouselEventCard from "./CarouselEventCard";
 
 export default function EventsCarousel() {
+    const [cardNumber, setCardNumber] = useState(0);
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    
+    let listOfCards = cards.slice(cardNumber).map((card) =>
+        <CarouselEventCard date={card} key={card}/>);
+    
+    //sprawdzenie ile elementów jest widocznych na stronie
+    // const x = () => {
+    //     listOfCards.forEach(element => console.log(element.style.gridRow));
+    // }
+    
+    // useEffect(() => {
+    //     x();
+    // })
+    //
+    const nextCard = () => {
+        if (cardNumber !== cards.length - 1) {
+            setCardNumber(cardNumber + 1);
+        }
+    }
+
+    const previousCard = () => {
+        if (cardNumber !== 0) {
+            setCardNumber(cardNumber - 1);
+        }
+    }
+
     return (
         <div className="carousel-container">
-            <button className="prev" id="nav-button" onClick={() => {console.log("prev")}}><ArrowBack/></button>
+            <button className="prev" id="nav-button" onClick={() => {
+                previousCard();
+            }}><ArrowBack/></button>
             <div className="track">
-                <div className="card">
-                    <img src={EventImage} alt='' style={{ width: "100%" }}/>
-                    <div>16-02-2022</div>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus id augue lobortis
-                        accumsan. Proin iaculis lacinia nibh in aliquet. Integer vestibulum aliquet lacinia.
-                    </div>
-                    <button style={{ border: "0"}}>Go for more!</button>
-                </div>
-                <div className="card">
-                    <img src={EventImage} alt='' style={{ width: "100%" }}/>
-                    <div>16-02-2022</div>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus id augue lobortis
-                        accumsan. Proin iaculis lacinia nibh in aliquet. Integer vestibulum aliquet lacinia.
-                    </div>
-                    <button style={{ border: "0"}}>Go for more!</button>
-                </div>
-                <div className="card">
-                    <img src={EventImage} alt='' style={{ width: "100%" }}/>
-                    <div>16-02-2022</div>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus id augue lobortis
-                        accumsan. Proin iaculis lacinia nibh in aliquet. Integer vestibulum aliquet lacinia.
-                    </div>
-                    <button style={{ border: "0"}}>Go for more!</button>
-                </div>
+                {listOfCards}
             </div>
-            <button className="next" id="nav-button" onClick={() => {console.log("next")}}><ArrowForward/></button>
+            <button className="next" id="nav-button" onClick={() => {
+                nextCard();
+            }}><ArrowForward/></button>
         </div>
-    )
+    );
 }
