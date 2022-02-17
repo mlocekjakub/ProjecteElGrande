9fef82115c9f7d106ecfef6c6da1adac3819be64
marcinZyla13 <82803199+marcinZyla13@users.noreply.github.com>
2022-02-17 10:41:23 +0100

    Jwt tokens - backend implementation

---
+++ KeepMovinAPI/KeepMovinAPI/Authentication/JwtAuthenticationManager.cs
@@ -0,0 +1,52 @@
+﻿using KeepMovinAPI.DAOs.Implementations;
+using KeepMovinAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+
+namespace KeepMovinAPI.Authentication
+{
+    public class JwtAuthenticationManager : IJwtAuthenticationManager
+    {
+        private readonly string key;
+
+        public JwtAuthenticationManager(string key)
+        {
+            this.key = key;
+     
+        }
+        public string Authenticate(User dataBaseUser, User loginUser,UserDao dao)
+        {
+
+            if (!dao.CompareUsers(dataBaseUser, loginUser))
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, loginUser.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                                        new SymmetricSecurityKey(tokenKey),
+                                       SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+            
+        }
+    }
+
+
+
+
+
+}
+
--- KeepMovinAPI/KeepMovinAPI/Startup.cs
+++ KeepMovinAPI/KeepMovinAPI/Startup.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using KeepMovinAPI.Controllers;
+using KeepMovinAPI.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace KeepMovinAPI
 {
@@ -26,16 +30,47 @@ namespace KeepMovinAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var key = "Tu ustawiamy nasz tajny klucz jakby co";
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme  = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+
+            });
+
+
+            services.AddSingleton<IJwtAuthenticationManager>(new JwtAuthenticationManager(key));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "KeepMovinAPI", Version = "v1"});
             });
+
+
             services.AddDbContext<KeepMovinDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<IDao<Event>, EventDao>();
             services.AddTransient<IDao<User>, UserDao>();
             services.AddTransient<UserController>();
             services.AddTransient<UserDao>();
+
+            
+        }
+
+        private SecurityKey SymmetricSecurityKey(object getBytes)
+        {
+            throw new NotImplementedException();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +86,8 @@ namespace KeepMovinAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
