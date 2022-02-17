fe53046a34000dd76cae7e5224aa4fccc24c242c
JakJar <kuba.jaroszewicz@gmail.com>
2022-02-17 22:36:58 +0100

    Updated WelcomePage, added carousel

--- keepmovin-view/src/components/WelcomePage/WelcomePageComponents/CarouselEventCard.js
+++ keepmovin-view/src/components/WelcomePage/WelcomePageComponents/CarouselEventCard.js
@@ -8,11 +8,11 @@ export default function CarouselEventCard( { date } ) {
                 maxWidth: "100%",
                 maxHeight: "100%"
             }}/>
-            <div>{date}</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus id augue lobortis
+            <div style={{ backgroundColor: "rgba(0, 0, 0, 0.7)", color: "white"}}>{date}</div>
+            <div style={{ backgroundColor: "rgba(0, 0, 0, 0.6)", color: "white"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus id augue lobortis
                 accumsan. Proin iaculis lacinia nibh in aliquet. Integer vestibulum aliquet lacinia.
             </div>
-            <button style={{border: "0"}}>Go for more!</button>
+            <button style={{border: "0", backgroundColor: "rgba(0, 0, 0, 0.75)", color: "white"}}>Go for more!</button>
         </div>
     );
 }
--- keepmovin-view/src/components/WelcomePage/WelcomePageComponents/EventsCarousel.js
+++ keepmovin-view/src/components/WelcomePage/WelcomePageComponents/EventsCarousel.js
@@ -1,43 +1,47 @@
-import React, {useEffect, useState} from 'react';
 import './EventCarouselStyles.css';
 import {ArrowBack, ArrowForward} from "@material-ui/icons";
 import CarouselEventCard from "./CarouselEventCard";
+import {useEffect, useState} from "react";
 
 export default function EventsCarousel() {
-    const [cardNumber, setCardNumber] = useState(0);
     const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const baseTransform = 20;
     
-    let listOfCards = cards.slice(cardNumber).map((card) =>
-        <CarouselEventCard date={card} key={card}/>);
-    
-    //sprawdzenie ile elementów jest widocznych na stronie
-    // const x = () => {
-    //     listOfCards.forEach(element => console.log(element.style.gridRow));
-    // }
-    
-    // useEffect(() => {
-    //     x();
-    // })
-    //
+    const [multiplier, setMultiplier] = useState(0);
+    const [transformValue, setTransformValue] = useState(baseTransform * multiplier)
+
+    let listOfCards = cards.map((card) =>
+        <div className='card-container' key={card}><CarouselEventCard date={card} key={card}/></div>)
+
     const nextCard = () => {
-        if (cardNumber !== cards.length - 1) {
-            setCardNumber(cardNumber + 1);
+        console.log("next");
+        if (multiplier < cards.length - 3) {
+            setMultiplier(multiplier + 1);
+            console.log(multiplier);
         }
     }
 
     const previousCard = () => {
-        if (cardNumber !== 0) {
-            setCardNumber(cardNumber - 1);
+        console.log("prev");
+        if (multiplier >= 1) {
+            setMultiplier(multiplier - 1);
+            console.log(multiplier);
         }
     }
+
+    useEffect(() => {
+        setTransformValue(baseTransform * multiplier);
+    }, [multiplier])
 
     return (
         <div className="carousel-container">
             <button className="prev" id="nav-button" onClick={() => {
                 previousCard();
             }}><ArrowBack/></button>
-            <div className="track">
-                {listOfCards}
+            <div className="carousel-inner">
+                <div className="track" style={{transform: `translateX(-${transformValue}rem)`}}>
+                    {listOfCards}
+                </div>
             </div>
             <button className="next" id="nav-button" onClick={() => {
                 nextCard();
