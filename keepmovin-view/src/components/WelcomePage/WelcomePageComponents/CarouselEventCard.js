2e264f8ff7d94982157dccebec57b72f288d7c53
JakJar <kuba.jaroszewicz@gmail.com>
2022-02-25 18:55:48 +0100

    Welcome page connected with api, fixed css styles

--- keepmovin-view/src/components/WelcomePage/WelcomePageComponents/CarouselEventCard.js
+++ keepmovin-view/src/components/WelcomePage/WelcomePageComponents/CarouselEventCard.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import EventImage from "../../../Images/News-Trailer-Web-4Sep20.png";
+import EventImage from "../../../Images/News-Trailer-Web-4Sep20.png"; //default image  
 
-export default function CarouselEventCard( { date } ) {
+export default function CarouselEventCard({eventId, eventTitle, startDate, eventInfo, eventPicture}) { //eventId, eventTitle for future implementation
+    if (eventPicture == null) {
+        eventPicture = EventImage; //remove when events will have image   
+    }
+
     return (
         <div className="card">
-            <img src={EventImage} alt='' style={{
+            <img src={eventPicture} alt='' style={{
                 maxWidth: "100%",
                 maxHeight: "100%"
             }}/>
-            <div style={{ backgroundColor: "rgba(0, 0, 0, 0.7)", color: "white"}}>{date}</div>
-            <div style={{ backgroundColor: "rgba(0, 0, 0, 0.6)", color: "white"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus id augue lobortis
-                accumsan. Proin iaculis lacinia nibh in aliquet. Integer vestibulum aliquet lacinia.
+            <div style={{backgroundColor: "rgba(0, 0, 0, 0.7)", color: "white"}}>
+                {startDate.slice(0, 10)}
+            </div>
+            <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)", color: "white"}}>
+                {eventInfo}
             </div>
             <button style={{border: "0", backgroundColor: "rgba(0, 0, 0, 0.75)", color: "white"}}>Go for more!</button>
         </div>
