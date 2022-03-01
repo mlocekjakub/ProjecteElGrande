c2d5abeb8da722ddff4d8d141822581e29f1e34f
JakJar <kuba.jaroszewicz@gmail.com>
2022-03-01 14:48:19 +0100

    Fix SettingsDao

--- KeepMovinAPI/KeepMovinAPI/DAOs/Implementations/SettingDao.cs
+++ KeepMovinAPI/KeepMovinAPI/DAOs/Implementations/SettingDao.cs
@@ -1,6 +1,28 @@
-﻿namespace KeepMovinAPI.DAOs.Implementations
+﻿using System.Collections.Generic;
+using KeepMovinAPI.Models;
+
+namespace KeepMovinAPI.DAOs.Implementations
 {
-    public class SettingDao
+    public class SettingDao : ISettingDao
     {
+        public void Add(Setting item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Setting Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Setting> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
