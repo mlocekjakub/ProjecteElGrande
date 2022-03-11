610c2c56c6e60eba07ce982f66faaafacd51c93f
Filip Koniuszewski <filip7446@gmail.com>
2022-03-11 18:09:05 +0100

    Footer v1

--- keepmovin-view/src/components/Layout.js
+++ keepmovin-view/src/components/Layout.js
@@ -1,12 +1,14 @@
 ﻿import Navbar from "./Navbar";
 // import Footer from "./Footer";
 import {Outlet} from "react-router-dom";
+import Footer from "./Footer";
 
 export default function Layout() {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer/>
         </div>
     );
 }
