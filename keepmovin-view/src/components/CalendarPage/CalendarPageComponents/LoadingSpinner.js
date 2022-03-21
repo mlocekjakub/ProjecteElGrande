8c82bc10e3e498eff9437e9f88c9d7946124e223
JakJar <kuba.jaroszewicz@gmail.com>
2022-03-21 22:45:48 +0100

    Modified event modal, fix issue, added new styles

---
+++ keepmovin-view/src/components/CalendarPage/CalendarPageComponents/LoadingSpinner.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+export default function LoadingSpinner() {
+    return (
+        <div className="spinner-border" role="status">
+                <span className="sr-only">
+                </span>
+        </div>
+    );
+}
