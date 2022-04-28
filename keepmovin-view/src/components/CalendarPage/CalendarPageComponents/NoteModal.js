8bd803942dc591dc08f9f0c8d85500f268ec1c94
JakJar <kuba.jaroszewicz@gmail.com>
2022-04-28 19:30:40 +0200

    feat: added note modal form to note modal under create event button

--- keepmovin-view/src/components/CalendarPage/CalendarPageComponents/NoteModal.js
+++ keepmovin-view/src/components/CalendarPage/CalendarPageComponents/NoteModal.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './CalendarModalStyles.css';
 import CloseIcon from '@mui/icons-material/Close';
 import NoteList from "./NoteList";
+import NoteModalForm from "./NoteModalForm";
 
 export default function NoteModal({openNote, onClose, eventId}) {
+    const [openNoteForm, setOpenNoteForm] = useState(false);
+
+    const handleOpenNoteForm = () => {
+        setOpenNoteForm(true);
+    }
+
     if (openNote) {
         return (
-            <div className="event-calendar-modal" onClick={e => e.stopPropagation()}>
-                <div className="event-modal-content">
+            <div className="event-calendar-modal" onClick={onClose}>
+                <div className="event-modal-content" onClick={e => e.stopPropagation()}>
                     <div className="event-modal-buttons" style={{justifyContent: "flex-end"}}>
                         <div className="close-button" role="button" onClick={() => {
                             onClose();
+                            setOpenNoteForm(false);
                         }}>
                             <CloseIcon/>
                         </div>
@@ -21,9 +29,10 @@ export default function NoteModal({openNote, onClose, eventId}) {
                                  marginLeft: "0.5rem",
                                  marginRight: "0.5rem"
                              }}>
-                            <div className="create-note-button" role="button">
-                                Create note
-                            </div>
+                            {openNoteForm === false ?
+                                <div className="create-note-button" role="button" onClick={handleOpenNoteForm}>
+                                    Create note
+                                </div> : <NoteModalForm openForm={openNoteForm} eventId={eventId} onClose={onClose}/>}
                         </div>
                         <div className="note-modal-body" style={{padding: "0.5rem"}}>
                             <NoteList eventId={eventId}/>
--- keepmovin-view/src/components/CalendarPage/CalendarPageComponents/NoteModalForm.js
+++ keepmovin-view/src/components/CalendarPage/CalendarPageComponents/NoteModalForm.js
@@ -5,7 +5,7 @@ import TextField from "@mui/material/TextField";
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-export default function NoteModalForm({openNote, eventId, onClose}) {
+export default function NoteModalForm({openForm, eventId, onClose}) { // TODO change openNote to openForm, create handle openForm 
     const [noteTitle, setNoteTitle] = useState("");
     const [noteData, setNoteData] = useState("");
 
@@ -44,58 +44,38 @@ export default function NoteModalForm({openNote, eventId, onClose}) {
             })
     }
 
-    if (!openNote) {
+    if (!openForm) {
         return null;
     }
 
-    if (openNote) {
+    if (openForm) {
         return (
-            <div className="event-calendar-modal" onClick={e => e.stopPropagation()}>
-                <div className="event-modal-content">
-                    <div className="event-modal-buttons" style={{justifyContent: "flex-end"}}>
-                        <div className="close-button" role="button" onClick={() => {
-                            onClose();
-                            setNoteTitle("");
-                            setNoteData("");
-                        }}>
-                            <CloseIcon/>
-                        </div>
-                    </div>
-                    <div className="event-modal-event-info">
-                        <div className="note-modal-header"
-                             style={{
-                                 marginLeft: "0.5rem",
-                                 marginRight: "0.5rem"
-                             }}>
-                            <TextField
-                                id="note-title"
-                                label="Title" fullWidth
-                                name="Title"
-                                style={{backgroundColor: "white"}}
-                                onChange={handleTitleChange}
-                            />
-                        </div>
-                        <div className="note-modal-body" style={{padding: "0.5rem"}}>
-                            <CKEditor
-                                editor={ClassicEditor}
-                                config={{
-                                    toolbar: ["heading", "|", "bold", "italic", "link", "bulletedList", "numberedList", "undo", "redo"],
-                                    removePlugins: ["EasyImage", "ImageUpload", "MediaEmbed", "Table", "TableToolbar", "Indent", "BlockQuote"]
-                                }}
-                                data=""
-                                onChange={(event, editor) => {
-                                    handleMessageChange(editor.getData());
-                                }}
-                            />
-                        </div>
-                        <div className="event-modal-footer">
-                            <div role="button" onClick={() => {
-                                handleNoteSave();
-                            }}>
-                                Save note!
-                            </div>
-                        </div>
-                    </div>
+            <div className="note-form">
+                <div className="note-form-title"
+                     style={{
+                         marginLeft: "0.5rem",
+                         marginRight: "0.5rem"
+                     }}>
+                    <TextField
+                        id="note-title"
+                        label="Title" fullWidth
+                        name="Title"
+                        style={{backgroundColor: "white"}}
+                        onChange={handleTitleChange}
+                    />
+                </div>
+                <div className="note-form-message" style={{padding: "0.5rem"}}>
+                    <CKEditor
+                        editor={ClassicEditor}
+                        config={{
+                            toolbar: ["heading", "|", "bold", "italic", "link", "bulletedList", "numberedList", "undo", "redo"],
+                            removePlugins: ["EasyImage", "ImageUpload", "MediaEmbed", "Table", "TableToolbar", "Indent", "BlockQuote"]
+                        }}
+                        data=""
+                        onChange={(event, editor) => {
+                            handleMessageChange(editor.getData());
+                        }}
+                    />
                 </div>
             </div>
         );
