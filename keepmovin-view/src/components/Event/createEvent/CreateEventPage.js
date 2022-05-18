ec164c8b0a3531075fa9f30d2f76f89d0bc02506
Filip Koniuszewski <filip7446@gmail.com>
2022-05-19 00:13:45 +0200

    Added create-event progress-bar, next and previous states and design refactor

--- keepmovin-view/src/components/Event/createEvent/CreateEventPage.js
+++ keepmovin-view/src/components/Event/createEvent/CreateEventPage.js
@@ -13,7 +13,7 @@ import EventDate from "./EventDate";
 import defaultImage from "../../../Images/DefaultProfileImage.jpg"
 import axios from "axios";
 
-export default function CreateEventPage(props) {
+export default function CreateEventPage() {
     
     const [eventForm, setEventForm] = useState({
         name: "",
@@ -39,6 +39,10 @@ export default function CreateEventPage(props) {
     
     const [organiserName, setOrganiserName] = useState("");
     
+    const [progressState, setProgressState] = useState(1);
+
+    const theme = useSelector((state) => state.theme.value)
+    
     useEffect(() => {
         let isMounted = true;
         axios
@@ -55,11 +59,6 @@ export default function CreateEventPage(props) {
         }
     }, [routeChange])
     
-    useEffect(() => {
-        console.log(eventForm)
-    }, [eventForm])
-    
-    const theme = useSelector((state) => state.theme.value)
     
     function HandleSubmit(e) {
         e.preventDefault();
@@ -92,6 +91,16 @@ export default function CreateEventPage(props) {
         setEventForm(previousState)
     }
     
+    function IncrementProgress(e) {
+        e.preventDefault()
+        progressState < 4 && setProgressState(progressState + 1)
+    }
+    
+    function DecrementProgress(e) {
+        e.preventDefault()
+        progressState > 1 && setProgressState(progressState - 1)
+    }
+    
     
     
     return (
@@ -99,59 +108,100 @@ export default function CreateEventPage(props) {
             <form>
                 <div className="create__header">
                     <h6>Create Event</h6>
-                    <h3>Event Details</h3>
+                    <h3>
+                        {progressState === 1 && "Event Details"}
+                        {progressState === 2 && "Sport Preferences"}
+                        {progressState === 3 && "Location"}
+                        {progressState === 4 && "Price"}
+
+                    </h3>
                 </div>
-                <div className="create__organiser">
-                    <img src={defaultImage} alt="default-profile-image"/>
-                    <div className="create__organiser-info">
-                        <div className="create__organiser-nickname">{organiserName}</div>
-                        <div className="create__organiser-profile">
-                            Organiser -- your profile
+                {progressState === 1 && 
+                    <div className="create__organiser">
+                        <img src={defaultImage} alt="default-profile-image"/>
+                        <div className="create__organiser-info">
+                            <div className="create__organiser-nickname">{organiserName}</div>
+                            <div className="create__organiser-profile">
+                                Organiser -- your profile
+                            </div>
                         </div>
                     </div>
+                }
+                {progressState === 1 &&
+                    <>
+                        <EventName
+                            eventForm={eventForm}
+                            setEventForm={setEventForm}/>
+                        <EventAbout
+                            eventForm={eventForm}
+                            setEventForm={setEventForm}/>
+                        <h5> Event Date </h5>
+                        <EventDate 
+                            eventForm={eventForm} 
+                            setEventForm={setEventForm}/>
+                    </>
+                }
+                {progressState === 2 &&
+                    <>
+                        <EventSport 
+                            eventForm={eventForm} 
+                            setEventForm={setEventForm}/>
+                        <EventType 
+                            eventForm={eventForm} 
+                            setEventForm={setEventForm}/>
+                        <EventExperienceLevel 
+                            eventForm={eventForm} 
+                            setEventForm={setEventForm}/>
+                        <EventMaxParticipants 
+                            eventForm={eventForm} 
+                            setEventForm={setEventForm}/>
+                    </>
+                }
+                {progressState === 3 &&
+                    <EventLocation
+                        eventForm={eventForm}
+                        setEventForm={setEventForm}
+                        HandleCountry={HandleCountry}
+                        HandleCity={HandleCity}
+                        HandleZipCode={HandleZipCode}/>
+                    
+                }
+                {progressState === 4 &&
+                    <EventPrice
+                        eventForm={eventForm}
+                        setEventForm={setEventForm}/>
+                }
+                <hr className="create__form-separator" />
+                <div className="create__progress-bar">
+                    <div className="create__progress-piece create__progress-bar-filled" />
+                    <div className={`create__progress-piece ${progressState > 1 && 'create__progress-bar-filled'}`} />
+                    <div className={`create__progress-piece ${progressState > 2 && 'create__progress-bar-filled'}`} />
+                    <div className={`create__progress-piece ${progressState > 3 && 'create__progress-bar-filled'}`} />
                 </div>
-                <EventName 
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <h5> Event Date </h5>
-                <EventDate
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <EventAbout 
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <EventSport
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <EventType
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <EventExperienceLevel
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <EventMaxParticipants 
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <h5> Price </h5>
-                <EventPrice  
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}/>
-                <h5> Event Location </h5>
-                <EventLocation
-                    eventForm={eventForm}
-                    setEventForm={setEventForm}
-                    HandleCountry={HandleCountry}
-                    HandleCity={HandleCity}
-                    HandleZipCode={HandleZipCode}/>
-                <input type="submit"
-                       name="submit"
-                       id="create-event"
-                       onClick={(e) => HandleSubmit(e)}
-                       value="Create Event"/>
+                <div className="create__navigation-buttons">
+                    <button 
+                        disabled={progressState === 1}
+                        className={`create__previous-page-button ${progressState === 1 ? 'create__previous-inactive' : 'create__previous-active'}`} 
+                        onClick={e => DecrementProgress(e)}>
+                        Previous
+                    </button>
+                    {progressState !== 4 && 
+                        <button 
+                            className="create__next-page-button" 
+                            onClick={e => IncrementProgress(e)}>
+                            Next
+                        </button>
+                    }
+                    {progressState === 4 && 
+                        <input type="submit" 
+                               name="submit"
+                               className="create__submit-button"
+                               id="create-event"
+                               onClick={(e) => HandleSubmit(e)}
+                               value="Create Event"/>
+                    }
+                </div>
             </form>
-            <div className="create__event-preview">
-                
-            </div> 
         </div>
     );
 }
